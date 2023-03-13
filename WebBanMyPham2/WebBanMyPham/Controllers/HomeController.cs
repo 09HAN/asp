51e7da8130356b9a90ee907534acc245ce52aa29
Cao Ngọc Hân <caohan2001@gmail.com>
2023-03-13 23:37:05 +0700

    asp

---
+++ WebBanMyPham2/WebBanMyPham/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanMyPham.Context;
+using WebBanMyPham.Models;
+
+namespace WebBanMyPham.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebBanMyPhamEntities objWebBanMyPhamEntities = new WebBanMyPhamEntities();
+        public ActionResult Index()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            objHomeModel.ListCategory = objWebBanMyPhamEntities.Category.ToList();
+            objHomeModel.ListProduct = objWebBanMyPhamEntities.Product.ToList();
+            return View(objHomeModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
