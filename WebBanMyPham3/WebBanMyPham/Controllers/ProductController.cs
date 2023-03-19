9c44ec3954a1f65480bf3b0e468fa77da47220a7
Cao Ngọc Hân <caohan2001@gmail.com>
2023-03-20 00:54:04 +0700

    asp3

---
+++ WebBanMyPham3/WebBanMyPham/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanMyPham.Context;
+
+namespace WebBanMyPham.Controllers
+{
+    public class ProductController : Controller
+    {
+        WebBanMyPhamEntities objWebBanMyPhamEntities = new WebBanMyPhamEntities();
+        // GET: Product
+        public ActionResult Detail(int id)
+        {
+            var product=objWebBanMyPhamEntities.Product.Where(p=> p.Id == id).FirstOrDefault();
+            return View(product);
+        }
+    }
+}
